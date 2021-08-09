d3dcd4bcb00e108a9e7adfd2d2f35872ada783fc
JollyforThee <jollyforthee@gmail.com>
2021-08-09 22:27:53 +0800

    Direction changing

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+    public float rotationSpeed = 4f;
     Vector2 lastClickedPos;
     bool moving;
+    Vector2 movement;
     printf("poop");
     
     private void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(lastClickedPos, Vector2.zero);
 
@@ -23,7 +27,10 @@ public class PlayerMovement : MonoBehaviour
         if (moving && (Vector2)transform.position != lastClickedPos && hit.collider != null)
         {
             float step = speed * Time.deltaTime;
+            Quaternion rotationDirection = Quaternion.LookRotation(Vector3.forward, movement);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationDirection, rotationSpeed);
             transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+
         }
 
         else
